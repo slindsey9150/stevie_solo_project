ee1a9de6e20693733f1ada4d7fe61b63dc826453
slindsey9150 <stevie.lindsey.jr@gmail.com>
2024-02-06 10:07:01 -0600

    read through all the components to see what's already there

--- database.sql
+++ database.sql
@@ -7,3 +7,78 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE Characters (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  level INTEGER NOT NULL,
+  charisma INTEGER NOT NULL,
+  constitution INTEGER NOT NULL,
+  strength INTEGER NOT NULL,
+  dexterity INTEGER NOT NULL,
+  wisdom INTEGER NOT NULL,
+  intelligence INTEGER NOT NULL,
+  player BOOLEAN NOT NULL,
+  class_id INTEGER NOT NULL,
+  inventory INTEGER NOT NULL,
+  spells VARCHAR NOT NULL,
+  stats INTEGER NOT NULL,
+  in_campaign BOOLEAN NOT NULL,
+  backstory TEXT NOT NULL,
+  is_complete BOOLEAN NOT NULL,
+  notes TEXT NOT NULL,
+  race_id INTEGER NOT NULL
+);
+
+
+
+CREATE TABLE class (
+  id SERIAL PRIMARY KEY,
+  class VARCHAR NOT NULL,
+  description INTEGER NOT NULL
+);
+
+CREATE TABLE stats (
+  id SERIAL PRIMARY KEY,
+  name INTEGER NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE adventure (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  name INTEGER NOT NULL,
+  player_characters INTEGER NOT NULL,
+  non_player_characters INTEGER NOT NULL,
+  player_input_id INTEGER NOT NULL
+);
+
+CREATE TABLE races (
+  id SERIAL PRIMARY KEY,
+  race INTEGER NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE player_inputs (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  adventure_id INTEGER NOT NULL,
+  inputs INTEGER NOT NULL
+);
+
+CREATE TABLE friends (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  friend_id INTEGER NOT NULL,
+  friendship_approval INTEGER NOT NULL,
+  request INTEGER NOT NULL
+);
+
+CREATE TABLE inventory (
+  id VARCHAR PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  name VARCHAR NOT NULL,
+  requirements VARCHAR NOT NULL,
+  price INTEGER NOT NULL,
+  weight INTEGER NOT NULL
+);
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route Includes
 const userRouter = require('./routes/user.router');
+const dndRouter = require('./routes/dnd.router')
 
 // Express Middleware
 app.use(express.json());
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
+app.use('/api/rulebook', dndRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
