0f594df11d6c57f4a37060b6e4cbfc40ba8d0005
slindsey9150 <stevie.lindsey.jr@gmail.com>
2024-03-06 13:40:12 -0600

    cleaning up code

--- src/components/CreateManageCharacters/CreateManageCharacters.jsx
+++ src/components/CreateManageCharacters/CreateManageCharacters.jsx
@@ -4,7 +4,10 @@ import ManagerComponent from '../ManagerComponent/ManagerComponent';
 import ButtonRoute from '../ButtonRoute/ButtonRoute';
 import BackButton from '../BackButton/BackButton';
 import SingleCharacter from '../CharacterCreation/CharacterCreationTabs/SingleCharacter';
+
+
 export default function CreateManageCharacters () {
+   
 
     return (
         <>
@@ -40,7 +43,25 @@ route='/createcharacter'/>
 <ButtonRoute
 name='Delete Character'
 route='/deletecharacter'/>
-<br/><br/><br/><br/>
+<br/><br/><br/>
+<iframe src="https://www.youtube.com/watch?v=jn8hdcfbQhw"></iframe>
+
+{/* <ExampleComponent/> */}
+
+{/* <AudioRecorder/> */}
+{/* <WebcamComponent/> */}
+{/* <ToggleSwitch label="video or photo"/> */}
+
+{/* <WebcamCapture/>
+<WebcamVideo/>
+<GeoLocator/>
+<PhoneCamera/> */}
+{/* <ReactCameraPro/> */}
+{/* <input type="file" accept="image/*" capture="environment"/> */}
+{/* <input type="file" id="fileInput" multiple></input>
+<input type="file" id="audioInput" accept="audio/*" multiple></input>
+
+<input type="datetime-local" id="audioInput"/> */}
 
 <SingleCharacter/>
 </>
